d39d1737cd59d9544d2b629a761879164c656f31
gongzhaopeng <gongguochuanqi@gmail.com>
2024-06-17 13:40:08 +0800

    ...

--- src/FakeChat.js
+++ src/FakeChat.js
@@ -1,28 +1,43 @@
 import * as React from 'react';
 
+import {BrowserRouter, Routes, Route, Outlet} from 'react-router-dom'
+
 import Box from '@mui/material/Box';
 
 import backgroundImage from './assets/images/cosmic-shear.jpg'
 
 function FakeChat() {
     return (
-        <Box sx={{
-            backgroundImage: `url(${backgroundImage})`,
-            paddingLeft: 2,
-            paddingRight: 2,
-            paddingTop: 6,
-            paddingBottom: 36
-        }}>
-            <iframe
-                src='https://xiling.cloud.baidu.com/cloud/web?appId=i-qe5q7sk9na8m7&appKey=j3hq5kzws7vwtx3rs8t8&projectId=6657e8a316ed7222fa5e511d&configVersion=publish&chatMode=isStream&usingAudio=userMedia&directLogin=1&mode=inline&ttsSpeed=5&ttsPitch=5&videoBg=rgb(0,0,0,0)&showMessage=false'
-                allow="microphone *"
-                style={{
-                    border: 0
-                }}
-                width='100%'
-                height="480"
-            />
-        </Box>
+        <BrowserRouter>
+            <Routes>
+                <Route path='fake-chat' element={
+                    <Box sx={{
+                        backgroundImage: `url(${backgroundImage})`,
+                        paddingLeft: 2,
+                        paddingRight: 2,
+                        paddingTop: 6,
+                        paddingBottom: 36
+                    }}>
+                        <Outlet/>
+                    </Box>
+                }>
+                    <Route index element={
+                        <iframe
+                            src='https://xiling.cloud.baidu.com/cloud/web?appId=i-qe5q7sk9na8m7&appKey=j3hq5kzws7vwtx3rs8t8&projectId=6657e8a316ed7222fa5e511d&configVersion=publish&chatMode=isStream&usingAudio=userMedia&directLogin=1&mode=inline&ttsSpeed=5&ttsPitch=5&videoBg=rgb(0,0,0,0)&showMessage=false'
+                            allow="microphone *"
+                            style={{
+                                border: 0
+                            }}
+                            width='100%'
+                            height="480"
+                        />
+                    }/>
+                    <Route path='tts/custom-voice' element={
+                        <h2> Hey, it's Ke Wang speaking! </h2>
+                    }/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
